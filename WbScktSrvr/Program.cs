2c7647c057f45c3fac00577c373e2f48fbe80abd
kdchk <kdchk.net@gmail.com>
2017-02-17 20:55:46 +0200

    [works] with debug comments

--- WbScktSrvr/Program.cs
+++ WbScktSrvr/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
+
+
 namespace WbScktSrvr
 {
     public class Program
--- WbScktSrvr/Startup.cs
+++ WbScktSrvr/Startup.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 
 namespace WbScktSrvr
 {
@@ -28,9 +28,48 @@ namespace WbScktSrvr
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseWebSockets();
+
+            app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var http = (HttpContext)context;
+
+                if (http.WebSockets.IsWebSocketRequest)
+                {
+
+                    Console.WriteLine("Console says: Connected");
+                    
+                   
+                    WebSocket webSocket = await http.WebSockets.AcceptWebSocketAsync();
+                    Console.WriteLine("1-WebSocket webSocket = await http.WebSockets.AcceptWebSocketAsync();");
+
+                    while (webSocket.State == WebSocketState.Open)
+                    {
+                        var token = CancellationToken.None; Console.WriteLine("2-var token: " + token);
+
+                        var buffer = new ArraySegment<Byte>(new Byte[20]); Console.WriteLine("3-var buffer: " + buffer); //initial value is 4096
+
+                        //code below plays after "Send" in "Smart Websocket Client"
+                        var received = await webSocket.ReceiveAsync(buffer, token); Console.WriteLine("4-var received: " + received);
+
+                        switch (received.MessageType)
+                        {
+                            case WebSocketMessageType.Text:
+
+                                Console.WriteLine("5-case WebSocketMessageType.Text: ");
+                                var request = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);   Console.WriteLine("6-var request: " + request);
+                                Console.WriteLine("MESSAGE: "+ request);
+                                var type = WebSocketMessageType.Text;    Console.WriteLine("7-var type: " + type);
+                                var data = Encoding.UTF8.GetBytes("Echo from server: " + request);   Console.WriteLine("8-var data: " + data);
+                                buffer = new ArraySegment<Byte>(data); Console.WriteLine("9-buffer: " + buffer);
+                                await webSocket.SendAsync(buffer, type, true, token);
+                                Console.WriteLine("10-switch (received.MessageType)");
+                                Console.WriteLine(" ");
+                                break;
+                        }
+                    }
+
+                }
             });
         }
     }
